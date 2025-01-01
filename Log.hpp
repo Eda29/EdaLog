accdc7fe882c558a0e7fcbdfc7f68b578ebe2baf
Eda29 <105377596+Eda29@users.noreply.github.com>
2025-01-01 11:12:39 -0500

    Add files.

---
+++ Log.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <string>
+
+namespace Eda::Util {
+enum class LogInfoLevel { INFO, WARNING, ERROR, CRITICAL };
+
+static std::map<LogInfoLevel, std::string> levelTexts = {
+    {LogInfoLevel::INFO, "[INFO]: "},
+    {LogInfoLevel::WARNING, "[WARNING] "},
+    {LogInfoLevel::ERROR, "[ERROR] "},
+    {LogInfoLevel::CRITICAL, "[CRITICAL ERROR] "}};
+
+class Logger {
+public:
+  static void Log(LogInfoLevel level, std::string msg) {
+    static std::ofstream logfile("log.txt");
+
+    std::cout << levelTexts[level] << msg << std::endl;
+    if (logfile.is_open()) {
+      logfile << levelTexts[level] << msg << std::endl;
+    }
+  }
+};
+} // namespace Eda::Util
